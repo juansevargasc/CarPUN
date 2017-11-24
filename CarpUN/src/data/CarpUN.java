3ec571824c732596acb25e5f665e0592e3589dbc
juanvargas <juanvargas@192.168.0.11>
2017-11-24 12:40:32 -0500

    Version 5

--- CarpUN/src/data/CarpUN.java
+++ CarpUN/src/data/CarpUN.java
@@ -13,18 +13,54 @@ import java.util.ArrayList;
  */
 public class CarpUN 
 {
-    ArrayList<Usuario> usuarios;
+    private ArrayList<Usuario> usuarios;
+    private int numeroUsuarios;
 
     public CarpUN() 
     {
         this.usuarios = new ArrayList<>();
+        this.numeroUsuarios = 0;
     }
     
     public boolean addUsuario(String nombre, String nombrDeUsuario, String clave, 
             String universidad, long telefono, String correo)
     {
         Usuario usuario = new Usuario(nombre, nombrDeUsuario, clave, universidad, telefono, correo);
+        this.numeroUsuarios++;
         return this.usuarios.add(usuario);   
     }
+    
+    /*
+    public boolean crearConductor(String nombrUsuario)
+    {
+        
+        Conductor conductor = new Conductor(nombrUsuario, nombrUsuario);
+        
+    }
+    */
+    
+    public Usuario getUsusario(String nombrUsuario)
+    {
+        for (Usuario usuario : this.usuarios) 
+        {
+            if( usuario.getNombrDeUsuario().equals(nombrUsuario) )
+            {
+                return usuario;
+            }
+        }
+        return null;
+    }
+    
+    //Getters and Setters
+    public ArrayList<Usuario> getUsuarios()
+    {
+        return usuarios;
+    }
+
+    public int getNumeroUsuarios() 
+    {
+        return numeroUsuarios;
+    }
+    
     
 }
